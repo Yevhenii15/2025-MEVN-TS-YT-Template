e40abcd955afa8754097207d6eb5944256dcda81
Yevhenii <yevmot01@easv365.dk>
2025-02-17 12:31:42 +0100

    made fetch of products

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template class="w-screen">
   <header class="text-white py-4">
-    <div class="wrapper" >
-
+    <div class="wrapper">
       <nav class="">
         <RouterLink to="/" class="mr-4">Home</RouterLink>
         <RouterLink to="/about" class="mr-4">About</RouterLink>
+        <RouterLink to="/products" class="mr-4">Products</RouterLink>
 
         <!-- RouterLink to products, Auth, Admin & and logout button -->
 
-
         <!-- toggle cart button -->
 
-
         <!-- Routerlink to orders -->
-
-
       </nav>
     </div>
   </header>
@@ -22,15 +18,11 @@
   <RouterView />
 
   <!-- CartBasket component -->
-
-
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
-
 
 <style scoped>
 .logo {
